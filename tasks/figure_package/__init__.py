edc1d3c05a379d7ceb1d5fbd01b6da1a62aa6e6a
bickrosss <madinatekeeva2006@gmail.com>
2025-11-24 01:28:43 +0300

    Create __init__.py

---
+++ tasks/figure_package/__init__.py
@@ -0,0 +1,4 @@
+from .figure import Figure, Rectangle, Circle, Trapezium, Reader, demonstrate_virtual_call
+
+__version__ = "1.0.0"
+__all__ = ["Figure", "Rectangle", "Circle", "Trapezium", "Reader", "demonstrate_virtual_call"]
