b3a1157efbe30473c618c7ee4e814bc146f438ff
bickrosss <madinatekeeva2006@gmail.com>
2025-12-11 11:48:14 +0300

    Update pair.py

--- tasks/pair.py
+++ tasks/pair.py
@@ -1,47 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from math import gcd
-
 class Pair:
+    BASE = 1000
+    
     def __init__(self, first=0, second=0):
-        self.__first = int(first)
-        self.__second = int(second)
+        self.first = first
+        self.second = second
     
-    @property
-    def first(self):
-        return self.__first
+    def set_first(self, value):
+        self.first = value
     
-    @first.setter
-    def first(self, value):
-        self.__first = int(value)
+    def set_second(self, value):
+        self.second = value
     
-    @property
-    def second(self):
-        return self.__second
+    def get_first(self):
+        return self.first
     
-    @second.setter
-    def second(self, value):
-        self.__second = int(value)
-    
-    def read(self):
-        self.__first = int(input("Введите первое число: "))
-        self.__second = int(input("Введите второе число: "))
-    
-    def display(self):
-        print(f"({self.__first}, {self.__second})")
+    def get_second(self):
+        return self.second
     
     def add(self, other):
-        if isinstance(other, Pair):
-            return Pair(self.__first + other.__first, self.__second + other.__second)
-        else:
-            raise ValueError("Операнд должен быть типа Pair")
+        return Pair(self.first + self.second, other.first + other.second)
     
     def __add__(self, other):
         return self.add(other)
     
     def __str__(self):
-        return f"({self.__first}, {self.__second})"
+        return f"({self.first}, {self.second})"
 
 
 class Long(Pair):
@@ -64,118 +50,49 @@ class Long(Pair):
     def low(self, value):
         self.second = value
     
-    def read(self):
-        self.high = int(input("Введите старшую часть числа: "))
-        self.low = int(input("Введите младшую часть числа: "))
-    
-    def display(self):
-        print(f"Старшая часть={self.high}, младшая часть={self.low}")
-    
     def add(self, other):
-        if isinstance(other, Long):
-            new_high = self.high + other.high
-            new_low = self.low + other.low
-            
-            # Обработка переполнения младшей части
-            if new_low >= 1000:
-                new_high += new_low // 1000
-                new_low = new_low % 1000
-            
-            return Long(new_high, new_low)
-        else:
-            raise ValueError("Операнд должен быть типа Long")
+
+        new_low = self.low + other.low
+        new_high = self.high + other.high
+        
+        if new_low >= self.BASE:
+            new_high += new_low // self.BASE
+            new_low = new_low % self.BASE
+        
+        return Long(new_high, new_low)
     
     def multiply(self, other):
-        if isinstance(other, Long):
-            # Упрощенное умножение (для демонстрации)
-            total_high = self.high * other.high
-            total_low = self.low * other.low
-            
-            # Нормализация
-            if total_low >= 1000:
-                total_high += total_low // 1000
-                total_low = total_low % 1000
-            
-            return Long(total_high, total_low)
-        else:
-            raise ValueError("Операнд должен быть типа Long")
+
+        num1 = self.high * self.BASE + self.low
+        num2 = other.high * self.BASE + other.low
+        result = num1 * num2
+        
+        new_high = result // self.BASE
+        new_low = result % self.BASE
+    
+        if new_low >= self.BASE:
+            new_high += new_low // self.BASE
+            new_low = new_low % self.BASE
+        
+        return Long(new_high, new_low)
     
     def subtract(self, other):
-        if isinstance(other, Long):
-            new_high = self.high - other.high
-            new_low = self.low - other.low
-            
-            # Обработка отрицательной младшей части
-            if new_low < 0:
-                new_high -= 1
-                new_low += 1000
-            
-            return Long(new_high, new_low)
-        else:
-            raise ValueError("Операнд должен быть типа Long")
+        num1 = self.high * self.BASE + self.low
+        num2 = other.high * self.BASE + other.low
+        
+        if num1 < num2:
+            raise ValueError("Результат не может быть отрицательным")
+        
+        result = num1 - num2
+        return Long(result // self.BASE, result % self.BASE)
     
     def __mul__(self, other):
         return self.multiply(other)
     
     def __sub__(self, other):
         return self.subtract(other)
+    
+    def __str__(self):
+        return f"{self.high}.{self.low:03d}"
 
 
-if __name__ == '__main__':
-    print("Демонстрация класса Pair")
-    p1 = Pair(10, 20)
-    p2 = Pair(5, 15)
-    
-    print("Пары чисел:")
-    print(f"p1 = ", end="")
-    p1.display()
-    print(f"p2 = ", end="")
-    p2.display()
-    
-    result_pair = p1 + p2
-    print("Сложение пар: ", end="")
-    result_pair.display()
-    
-    print("\nДемонстрация класса Long")
-    l1 = Long(2, 500)
-    l2 = Long(1, 600)
-    
-    print("Long числа:")
-    l1.display()
-    l2.display()
-    
-    result_add = l1 + l2
-    print("Сложение Long: ", end="")
-    result_add.display()
-    
-    result_mult = l1 * l2
-    print("Умножение Long: ", end="")
-    result_mult.display()
-    
-    result_sub = l1 - l2
-    print("Вычитание Long: ", end="")
-    result_sub.display()
-    
-    print("\nТестирование переполнения")
-    l3 = Long(1, 800)
-    l4 = Long(1, 300)
-    
-    print("Тест переполнения:")
-    l3.display()
-    l4.display()
-    
-    result_overflow = l3 + l4
-    print("Результат (с обработкой переполнения): ", end="")
-    result_overflow.display()
-    
-    print("Создадим новую пару через ввод:")
-    p3 = Pair()
-    p3.read()
-    print("Введенная пара: ", end="")
-    p3.display()
-    
-    print("Создадим новое Long число через ввод:")
-    l5 = Long()
-    l5.read()
-    print("Введенное число: ", end="")
-    l5.display()
